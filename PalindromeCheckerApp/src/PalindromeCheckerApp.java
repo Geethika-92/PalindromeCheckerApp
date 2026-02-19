73ae1d0cf82f40418dab9a68dc1051076bd22411
Sai Pavan <saipavan7974@gmail.com>
2026-02-19 10:09:23 +0530

    Geethika Added UseCase2

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,11 +1,31 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
 
-    /*
-    UC1
-     */
+/**
+ * @author Developer
+ * @version 2.0
+ */
+class UseCase2PalindromeCheckerApp {
+
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String input = sc.nextLine();
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        sc.close();
     }
 }
