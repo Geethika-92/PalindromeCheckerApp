9ecc14b379057a6dce26684b77b75661e84f0628
Geethika-92 <geethikatalari927@gmail.com>
2026-02-26 09:49:34 +0530

    UC12 - Strategy Pattern implementation for palindrome checker

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,38 +1,81 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
+
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean isPalindrome(String input);
+}
+
+// Stack Strategy Implementation
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push all characters into stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        // Compare while popping
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Deque Strategy Implementation
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String input) {
+
+        Deque<Character> deque = new LinkedList<>();
+
+        // Add characters to deque
+        for (char c : input.toCharArray()) {
+            deque.add(c);
+        }
+
+        // Compare from both ends
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Main Class
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "racecar";
+        String input = "level";
 
-        // Create service object
-        PalindromeService service = new PalindromeService();
+        // Choose strategy dynamically
+        PalindromeStrategy strategy;
 
-        boolean result = service.checkPalindrome(input);
+        // Change this to test different strategies
+        String selectedStrategy = "STACK"; // or "DEQUE"
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-    }
-}
-
-// Service class containing palindrome logic
-class PalindromeService {
-
-    public boolean checkPalindrome(String input) {
-
-        int start = 0;
-        int end = input.length() - 1;
-
-        // Compare characters moving inward
-        while (start < end) {
-
-            if (input.charAt(start) != input.charAt(end)) {
-                return false;
-            }
-
-            start++;
-            end--;
+        if (selectedStrategy.equalsIgnoreCase("STACK")) {
+            strategy = new StackStrategy();
+        } else {
+            strategy = new DequeStrategy();
         }
 
-        return true;
+        boolean result = strategy.isPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Using Strategy : " + selectedStrategy);
+        System.out.println("Is Palindrome? : " + result);
     }
 }
